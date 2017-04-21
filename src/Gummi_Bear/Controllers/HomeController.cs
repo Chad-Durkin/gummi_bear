092035379b487166c5f90358c0d24797d0f21768
Chad Durkin <Chaddurkin@gmail.com>
2017-04-21 11:34:24 -0700

    add migration database

--- src/Gummi_Bear/Controllers/HomeController.cs
+++ src/Gummi_Bear/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Gummi_Bear.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
---
+++ src/Gummi_Bear/Model/GummiBearDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gummi_Bear.Model
+{
+    public class GummiBearDbContext : DbContext
+    {
+        public GummiBearDbContext()
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummiBearWithMigrations;integraded security=True");
+        }
+        public GummiBearDbContext(DbContextOptions<GummiBearDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
